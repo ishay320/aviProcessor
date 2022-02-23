8c297c4f5112fd059449f8d271f7d76be7a9ec20
ishay320 <ishay320@hotmail.com>
2022-02-23 16:09:08 +0200

    change the codec to h.264 and cleanup of the code

--- main.cpp
+++ main.cpp
@@ -8,12 +8,13 @@
 
 double randomWait() {
     double r = ((double)rand() / (RAND_MAX));
-    usleep(r * 60 * 1000); // Sleep for a random time in range of 0-60 ms
+    usleep(r * 60 * 1000); // sleep for a random time in range of 0-60 ms
     return (r * 60 * 1000);
 }
 
 void usage(std::ostream &stream, std::string file_name) { stream << "usage: " << file_name << " <input file> <output>\n"; }
 
+// TODO: add buffer and run processing in another thread
 int main(int argc, char const *argv[]) {
     if (argc < 3) {
         usage(std::cout, argv[0]);
@@ -22,44 +23,44 @@ int main(int argc, char const *argv[]) {
     std::string input_file = argv[1];
     std::string output_file = argv[2];
 
-    // Create a VideoCapture object and open the input file
+    // create a VideoCapture object and open the input file
     cv::VideoCapture cap(input_file);
 
-    // Check if opened successfully
+    // check if opened successfully
     if (!cap.isOpened()) {
         std::cout << "Error opening video stream or file" << std::endl;
         return 1;
     }
 
-    // resolutions of the frame
+    // get resolutions of the input video
     int frame_width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
     int frame_height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
 
-    // Define the codec and create VideoWriter object
-    cv::VideoWriter video(output_file, cv::VideoWriter::fourcc('D', 'I', 'V', 'X'), 10, cv::Size(frame_width, frame_height));
+    // define the codec and create VideoWriter object
+    cv::VideoWriter video(output_file, cv::VideoWriter::fourcc('a', 'v', 'c', '1'), 10, cv::Size(frame_width, frame_height));
 
     while (true) {
         // TODO: read every 40ms - maybe use delta time to wait for next frame
 
         cv::Mat frame;
-        // Capture frame-by-frame
+        // capture frame-by-frame
         cap >> frame;
 
-        // If the frame is empty, break immediately
+        // if the frame is empty, the file ended
         if (frame.empty())
             break;
 
         cv::putText(frame, "first blender video ;)", cv::Point(0, 20), cv::FONT_HERSHEY_DUPLEX, 0.7, cv::Scalar(100, 255, 0), 1, false);
-        // Write the frame to file
-        video.write(frame);
+        // write the frame to file
+        video.write(frame); // FIXME: it save the wait time so the length is bigger, maybe skip frames or save in ram
 
         randomWait();
 
         // TODO: add option to turn on and off maybe -s for show
-        // Display the resulting frame
-        imshow("Frame", frame); // FIXME: it save the wait time so the length is bigger, maybe skip frames or save in ram
+        // display the resulting frame
+        imshow("Frame", frame);
 
-        // Press ESC on keyboard to exit
+        // press ESC on keyboard to exit
         char c = (char)cv::waitKey(ESC);
         if (c == 27)
             break;
@@ -69,7 +70,7 @@ int main(int argc, char const *argv[]) {
     cap.release();
     video.release();
 
-    // Closes frame
+    // closes frame
     cv::destroyAllWindows();
 
     return 0;
