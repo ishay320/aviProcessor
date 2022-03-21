f6def6db13f6c8525c40940258ef4bcda50d8543
ishay320 <ishay320@hotmail.com>
2022-03-21 11:52:28 +0200

    parser now can do simple parsing
    can parse int, string, bool, float
    need to add errors

--- parser.cpp
+++ parser.cpp
@@ -65,23 +65,145 @@ int parser(int argc, char const *argv[], std::string &input_file, std::string &o
     return 0;
 }
 
-// string: token | string: default | string: usage
-std::vector<std::vector<std::string>> g_keys_vec;
-// TODO: save the container maybe add: type and void* to g_keys_vec
+class Token {
+    // for parsing: int | bool | string | float
+  private:
+    char type;
+    bool *b;
+    int *i;
+    std::string *s;
+    float *f;
+
+    std::string token;
+    std::string usage;
+
+  public:
+    Token(std::string token, std::string usage, bool *b, int *i, std::string *s, float *f) {
+        this->b = b;
+        this->i = i;
+        this->s = s;
+        this->f = f;
+
+        this->token = token;
+        this->usage = usage;
+    }
+
+    // TODO: check for size of token: -- or - or custom
+    std::string getToken() { return "-" + this->token; }
+    std::string getUsage() { return this->usage; }
+
+    char getType() {
+        if (this->b != nullptr) {
+            return 'b';
+        } else if (this->i != nullptr) {
+            return 'i';
+        } else if (this->s != nullptr) {
+            return 's';
+        } else if (this->f != nullptr) {
+            return 'f';
+        } else {
+            return '?';
+        }
+    }
+    bool getBool() { return *(this->b); }
+    int getInt() { return *(this->i); }
+    std::string getString() { return *(this->s); }
+    float getFloat() { return *(this->f); }
+
+    void setBool(bool b) { *(this->b) = b; }
+    void setInt(int i) { *(this->i) = i; }
+    void setString(std::string s) { *(this->s) = s; }
+    void setFloat(float f) { *(this->f) = f; }
+};
+
+std::vector<Token> g_container_vec;
+
 void parseExist(std::string token, std::string usage, bool *container) {
-    std::vector<std::string> key;
-    key.push_back(token);
-    key.push_back("false");
-    key.push_back(usage);
-    g_keys_vec.push_back(key);
+    Token con{token, usage, container, nullptr, nullptr, nullptr};
+    g_container_vec.push_back(con);
+}
+
+void parseString(std::string token, std::string default_opt, std::string usage, std::string *container) {
+    Token con{token, usage, nullptr, nullptr, container, nullptr};
+    con.setString(default_opt);
+    g_container_vec.push_back(con);
+}
+
+void parseInt(std::string token, int default_opt, std::string usage, int *container) {
+    Token con{token, usage, nullptr, container, nullptr, nullptr};
+    con.setInt(default_opt);
+    g_container_vec.push_back(con);
+}
+
+void parseFloat(std::string token, float default_opt, std::string usage, float *container) {
+    Token con{token, usage, nullptr, nullptr, nullptr, container};
+    con.setFloat(default_opt);
+    g_container_vec.push_back(con);
 }
 
 void parserPrint(void) {
-    for (auto &&i : g_keys_vec) {
-        for (auto &&j : i) {
-            std::cout << j << '\t';
+    std::cout << "***********\n";
+    for (auto &&i : g_container_vec) {
+        char type = i.getType();
+        switch (type) {
+        case 'b':
+            std::cout << "token: " << i.getToken() << " |usage: " << i.getUsage() << " |type: bool -> " << i.getBool() << std::endl;
+            break;
+        case 'i':
+            std::cout << "token: " << i.getToken() << " |usage: " << i.getUsage() << " |type: int -> " << i.getInt() << std::endl;
+            break;
+        case 's':
+            std::cout << "token: " << i.getToken() << " |usage: " << i.getUsage() << " |type: string -> " << i.getString() << std::endl;
+            break;
+        case 'f':
+            std::cout << "token: " << i.getToken() << " |usage: " << i.getUsage() << " |type: float -> " << i.getFloat() << std::endl;
+            break;
+        case '?':
+            std::cout << "token: " << i.getToken() << " |usage: " << i.getUsage() << " |type: ??????" << std::endl;
+            break;
+        default:
+            break;
         }
-        std::cout << '\n';
     }
-    std::cout << '\n';
+
+    std::cout << "***********\n";
+}
+
+void parseAll(int argc, char const *argv[]) {
+    for (int i = 1; i < argc; i++) {
+        for (auto &&key : g_container_vec) {
+            if (key.getToken().compare(argv[i]) == 0) {
+                switch (key.getType()) {
+                case 'b':
+                    key.setBool(true);
+                    break;
+                case 's':
+                    if (i < argc) {
+                        key.setString(argv[++i]);
+                    } else {
+                        // TODO: throw error
+                    }
+                    break;
+                case 'i':
+                    if (i < argc) {
+                        key.setInt(std::stoi(argv[++i]));
+                    } else {
+                        // TODO: throw error
+                    }
+                    break;
+                case 'f':
+                    if (i < argc) {
+                        key.setInt(std::stof(argv[++i]));
+                    } else {
+                        // TODO: throw error
+                    }
+                    break;
+                default:
+                    // TODO: throw error
+                    break;
+                }
+                break;
+            }
+        }
+    }
 }
--- parser.hpp
+++ parser.hpp
@@ -17,6 +17,17 @@ int parser(int argc, char const *argv[], std::string &input_file, std::string &o
  * @param container the container will updated in parse all
  */
 void parseExist(std::string token, std::string usage, bool *container);
+// TODO: add required option
+void parseString(std::string token, std::string default_opt, std::string usage, std::string *container);
+void parseInt(std::string token, int default_opt, std::string usage, int *container);
+void parseFloat(std::string token, float default_opt, std::string usage, float *container);
+
+/**
+ * @brief parse all the tokens that saved
+ *
+ * @param argc count of words
+ * @param argv array of words, with the first in the array the main file name
+ */
 void parseAll(int argc, char const *argv[]);
 
 /**
